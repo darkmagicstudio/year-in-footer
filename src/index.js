df61a33d9a7a5f4732c201fd54eea11059194f8a
killianog <83577130+killianog@users.noreply.github.com>
2022-11-04 22:09:54 +0000

    add

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() { 
+    const y = document.querySelector('[data-year-in-footer]');
+    const c = new Date().getFullYear();
+    y.textContent = c;
+  });
